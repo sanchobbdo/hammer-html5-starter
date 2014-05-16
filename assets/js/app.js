103c216522cbcfcff8a45e3c8570c0ab51802f89
Adrian Quevedo <adriancquevedo@sanchobbdo.com>
2014-05-16 12:24:24 -0500

    update js files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,48 +1,16 @@
 'use strict';
 
+
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
 config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-	$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-	$routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
-
-var NS = NS || {};
-NS.Site = (function(){
-	$(document).on('ready', function(){
-		// @todo Write here the Javascript
-
-		var NameSite = {
-			init: function () {
-				this.bindUIActions();
-
-				// Placeholder Polyfill
-				$('input, textarea').placeholder();
-			},
-			bindUIActions: function () {
-
-				$('#id').on('click', function (e) {
-					//actions
-					e.preventDefault();
-				});
-
-				//GOOGLE ANALYTICS EVENTS
-				$('.btn').on('click', function () {
-					var title = $(this).text();
-
-					ga('send', {
-						'hitType': 'event',          // Required.
-						'eventCategory': 'button',   // Required.
-						'eventAction': 'click',      // Required.
-						'eventLabel': 'Clic en: ' + title
-					});
-				});
-			}
-		};
-
-		NameSite.init();
-
-	});
-
-}());
